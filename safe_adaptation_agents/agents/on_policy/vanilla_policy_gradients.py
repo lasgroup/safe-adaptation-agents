37fba7fc582ee0bb52e35ebda89ddb6303cf70d6
yardenas <yardas@student.ethz.ch>
2022-05-14 19:49:25 +0200

    Seems like it's working

--- safe_adaptation_agents/agents/on_policy/vanilla_policy_gradients.py
+++ safe_adaptation_agents/agents/on_policy/vanilla_policy_gradients.py
@@ -71,40 +71,51 @@ class VanillaPolicyGrandients(Agent):
   def train(self, observation: np.ndarray, action: np.ndarray,
             reward: np.ndarray, *_):
     advantage, return_ = self._evaluate(self.critic.params, observation, reward)
-    for _ in range(self.config.update_steps):
-      (self.actor.state, self.critic.state,
-       report) = self._update_step(self.actor.state, self.critic.state,
-                                   observation[:, :-1], action, advantage,
-                                   return_, next(self.rng_seq))
+    self.actor.state, report = self.actor_update_step(self.actor.state,
+                                                      observation[:, :-1],
+                                                      action, advantage)
+    for k, v in report.items():
+      self.logger[k] = v
+    for _ in range(self.config.value_update_steps):
+      (self.critic.state,
+       report) = self.critic_update_step(self.critic.state, observation[:, :-1],
+                                         return_)
       for k, v in report.items():
         self.logger[k] = v
 
   @functools.partial(jax.jit, static_argnums=0)
-  def _update_step(self, actor_state: LearningState,
-                   critic_state: LearningState, observation: jnp.ndarray,
-                   actions: jnp.ndarray, advantage: jnp.ndarray,
-                   return_: jnp.ndarray,
-                   seed: jnp.ndarray) -> [LearningState, LearningState, dict]:
-    policy_loss, policy_grads = jax.value_and_grad(self.policy_loss)(
-        actor_state.params, observation, actions, advantage, seed)
-    new_actor_state = self.actor.grad_step(policy_grads, actor_state)
-    value_loss, value_grads = jax.value_and_grad(self.critic_loss)(
-        critic_state.params, observation, return_)
-    new_critic_state = self.critic.grad_step(value_grads, critic_state)
+  def critic_update_step(self, critic_state: LearningState,
+                         observation: jnp.ndarray,
+                         return_: jnp.ndarray) -> [LearningState, dict]:
+    loss, grads = jax.value_and_grad(self.critic_loss)(critic_state.params,
+                                                       observation, return_)
+    new_critic_state = self.critic.grad_step(grads, critic_state)
+    return new_critic_state, {
+        'agent/critic/loss': loss,
+        'agent/critic/grad': optax.global_norm(grads)
+    }
+
+  @functools.partial(jax.jit, static_argnums=0)
+  def actor_update_step(self, actor_state: LearningState,
+                        observation: jnp.ndarray, actions: jnp.ndarray,
+                        advantage: jnp.ndarray) -> [LearningState, dict]:
+    loss, grads = jax.value_and_grad(self.policy_loss)(actor_state.params,
+                                                       observation, actions,
+                                                       advantage)
+    new_actor_state = self.actor.grad_step(grads, actor_state)
     entropy = self.actor.apply(actor_state.params, observation).entropy().mean()
-    return new_actor_state, new_critic_state, {
-        'agent/actor/loss': policy_loss,
-        'agent/actor/grad': optax.global_norm(policy_grads),
-        'agent/actor/entropy': entropy,
-        'agent/critic/loss': value_loss,
-        'agent/critic/grad': optax.global_norm(value_grads)
+    return new_actor_state, {
+        'agent/actor/loss': loss,
+        'agent/actor/grad': optax.global_norm(grads),
+        'agent/actor/entropy': entropy
     }
 
   def policy_loss(self, actor_params: hk.Params, observation: jnp.ndarray,
-                  actions: jnp.ndarray, advantage: jnp.ndarray,
-                  seed: jnp.ndarray):
+                  actions: jnp.ndarray, advantage: jnp.ndarray):
     pi = self.actor.apply(actor_params, observation)
-    objective = pi.log_prob(actions) * advantage
+    objective = (
+        pi.log_prob(actions) * advantage +
+        self.config.entropy_regularization * pi.entropy())
     return -objective.mean()
 
   def critic_loss(self, critic_params: hk.Params, observation: jnp.ndarray,
