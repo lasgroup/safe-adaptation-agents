68ddaea542ee9a811a237c00a53306bc359b11a6
yardenas <yardas@student.ethz.ch>
2022-05-30 21:23:39 +0200

    Implementation of CheetahDirection test

--- safe_adaptation_agents/config.py
+++ safe_adaptation_agents/config.py
@@ -38,29 +38,9 @@ def resolve_agent(remaining, config_names, configs):
 def load_config(args: Optional[List[AnyStr]] = None):
   import argparse
   import ruamel.yaml as yaml
-
-  def args_type(default):
-
-    def parse_string(x):
-      if default is None:
-        return x
-      if isinstance(default, bool):
-        return bool(['False', 'True'].index(x))
-      if isinstance(default, int):
-        return float(x) if ('e' in x or '.' in x) else int(x)
-      if isinstance(default, (list, tuple)):
-        return tuple(args_type(default[0])(y) for y in x.split(','))
-      return type(default)(x)
-
-    def parse_object(x):
-      if isinstance(default, (list, tuple)):
-        return tuple(x)
-      return x
-
-    return lambda x: parse_string(x) if isinstance(x, str) else parse_object(x)
-
   parser = argparse.ArgumentParser()
   parser.add_argument('--configs', nargs='+', default=['defaults'])
+  # Load all config parameters and infer their types and default values.
   args, remaining = parser.parse_known_args(args)
   with open(os.path.join(BASE_PATH, 'configs.yaml')) as file:
     configs = yaml.safe_load(file)
@@ -69,19 +49,21 @@ def load_config(args: Optional[List[AnyStr]] = None):
     defaults.update(configs[name])
   defaults.update(resolve_agent(remaining, args.configs, configs))
   updated_remaining = []
+  # Collect all the user inputs that override the default parameters.
   for idx in range(0, len(remaining), 2):
     stripped = remaining[idx].strip('-')
+    # Allow the user to override specific values within dictionaries.
     if '.' in stripped:
       params_group, key = stripped.split('.')
-      orig_value = defaults[params_group][key]
-      arg_type = args_type(orig_value)
-      defaults[params_group][key] = arg_type(remaining[idx + 1])
+      # Override the default value within a dictionary.
+      defaults[params_group][key] = yaml.safe_load(remaining[idx + 1])
     else:
       updated_remaining.append(remaining[idx])
       updated_remaining.append(remaining[idx + 1])
   remaining = updated_remaining
   parser = argparse.ArgumentParser()
+  # Add arguments from the defaults to create the default parameters namespace.
   for key, value in sorted(defaults.items(), key=lambda x: x[0]):
-    arg_type = args_type(value)
-    parser.add_argument(f'--{key}', type=arg_type, default=arg_type(value))
+    parser.add_argument(f'--{key}', type=yaml.safe_load, default=value)
+  # Parse the remaining arguments into the parameters namespace.
   return validate_config(parser.parse_args(remaining))
--- tests/test_maml_ppo_lagrangian.py
+++ tests/test_maml_ppo_lagrangian.py
@@ -3,6 +3,9 @@ import pytest
 
 import numpy as np
 
+import gym
+from gym.envs.mujoco.mujoco_env import MujocoEnv
+
 from safe_adaptation_gym import benchmark
 
 from safe_adaptation_agents import agents
@@ -12,51 +15,101 @@ from safe_adaptation_agents import logging
 from safe_adaptation_agents.trainer import Trainer
 
 
-@pytest.fixture
-def agent_env_config():
-  config = options.load_config([
-      '--configs', 'defaults', 'domain_randomization', '--agent',
-      'maml_ppo_lagrangian', '--num_trajectories', '30', '--time_limit', '1000',
-      '--vf_iters', '80', '--pi_iters', '80', '--eval_trials', '1',
-      '--render_episodes', '0', '--train_driver.adaptation_steps', '1000',
-      '--train_driver.query_steps', '100', '--safe', 'True',
-      '--task_batch_size', '2'
-  ])
-  if not config.jit:
-    from jax.config import config as jax_config
-    jax_config.update('jax_disable_jit', True)
+class DummyBenchmark:
 
-  def make_env(config):
-    import safe_adaptation_gym
-    from gym.wrappers import TimeLimit
-    env = safe_adaptation_gym.make(config.robot)
-    env = TimeLimit(env, config.time_limit)
-    return env
+  def __init__(self, batch_size: int):
+    self._batch_size = batch_size
+    self.rs = np.random.RandomState(666)
 
-  env = make_env(config)
-  agent: maml_ppo_lagrangian.MamlPpoLagrangian = agents.make(
-      config, env.observation_space, env.action_space,
-      logging.TrainingLogger(config.log_dir))
-  return agent, env, config
+  def _sample_tasks(self):
+    return self.rs.choice((
+        -1.,
+        1,
+    ), (self._batch_size,))
+
+  @property
+  def train_tasks(self):
+    return self.test_tasks
+
+  @property
+  def test_tasks(self):
+    tasks = self._sample_tasks()
+    for task in tasks:
+      yield str(task), task
 
 
-def test_adapt(agent_env_config):
-  agent, env, config = agent_env_config
-  obs = np.ones((
-      config.task_batch_size,
-      config.num_trajectories,
-      config.time_limit + 1,
-  ) + env.observation_space.shape)
-  rs = np.random.RandomState(666)
-  act = rs.random((config.task_batch_size, config.num_trajectories,
-                   config.time_limit) + env.action_space.shape)
-  reward = rs.random(
-      (config.task_batch_size, config.num_trajectories, config.time_limit))
-  cost = rs.random(
-      (config.task_batch_size, config.num_trajectories, config.time_limit))
-  agent.adapt(obs, act, reward, cost)
-  assert all(
-      task_posterior is not None for task_posterior in agent.pi_posterior)
+# Acknowledgment: https://github.com/jonasrothfuss/ProMP/blob
+# https://github.com/jonasrothfuss/ProMP/blob
+# /93ae339e23dfc6e1133f9538f2c7cc0ccee89d19/meta_policy_search/envs
+# /mujoco_envs/half_cheetah_rand_direc.py
+class HalfCheetahRandDirecEnv(MujocoEnv, gym.utils.EzPickle):
+
+  def __init__(self):
+    self.set_task(1.)
+    MujocoEnv.__init__(self, 'half_cheetah.xml', 5)
+    gym.utils.EzPickle.__init__(self)
+
+  def set_task(self, task):
+    """
+        Args:
+            task: task of the meta-learning environment
+        """
+    self.goal_direction = task
+
+  def get_task(self):
+    """
+        Returns:
+            task: task of the meta-learning environment
+        """
+    return self.goal_direction
+
+  def step(self, action):
+    xposbefore = self.sim.data.qpos[0]
+    self.do_simulation(action, self.frame_skip)
+    xposafter = self.sim.data.qpos[0]
+    ob = self._get_obs()
+    reward_ctrl = -0.5 * 0.1 * np.square(action).sum()
+    reward_run = self.goal_direction * (xposafter - xposbefore) / self.dt
+    reward = reward_ctrl + reward_run
+    done = False
+    return ob, reward, done, dict(
+        reward_run=reward_run, reward_ctrl=reward_ctrl)
+
+  def _get_obs(self):
+    return np.concatenate([
+        self.sim.data.qpos.flat[1:],
+        self.sim.data.qvel.flat,
+    ])
+
+  def reset_model(self):
+    qpos = self.init_qpos + self.np_random.uniform(
+        low=-.1, high=.1, size=self.model.nq)
+    qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
+    self.set_state(qpos, qvel)
+    return self._get_obs()
+
+  def viewer_setup(self):
+    self.viewer.cam.distance = self.model.stat.extent * 0.5
+
+  def reset(
+      self,
+      *,
+      seed=None,
+      return_info=False,
+      options=None,
+  ):
+    super().reset(seed=seed)
+    self.sim.reset()
+    ob = self.reset_model()
+    if options is not None and 'task' in options:
+      self.set_task(options['task'])
+    if not return_info:
+      return ob
+    else:
+      return ob, {}
+
+  def viewer_setup(self):
+    self.viewer.cam.distance = self.model.stat.extent * 0.5
 
 
 @pytest.mark.safe
@@ -87,3 +140,35 @@ def test_safe():
     objective, constraint = trainer.train()
   assert np.asarray(objective.values()).mean() > 5.
   assert all(value < config.cost_limit for value in constraint.values())
+
+
+@pytest.mark.not_safe
+def test_cheetah():
+
+  def make_env(config):
+    env = HalfCheetahRandDirecEnv()
+    env = gym.wrappers.TimeLimit(env, 100)
+    return env
+
+  config = options.load_config([
+      '--configs', 'defaults', '--agent', 'maml_ppo_lagrangian',
+      '--eval_trials', '0', '--epochs', '334', '--log_dir',
+      'results/test_maml_ppo_half_cheetah', '--task_batch_size', '40', '--safe',
+      'False', '--actor.layers', '[64, 64]', '--policy_inner_lr', '0.1',
+      '--time_limit', '100', '--num_trajectories', '20',
+      '--num_query_trajectories', '1', '--train_driver',
+      '{\'adaptation_steps\': 2000, \'query_steps\': 100}', '--test_driver',
+      '{\'adaptation_steps\': 2000, \'query_steps\': 100}'
+  ])
+  if not config.jit:
+    from jax.config import config as jax_config
+    jax_config.update('jax_disable_jit', True)
+  path = os.path.join(config.log_dir, 'state.pkl')
+  task_sampler = DummyBenchmark(config.task_batch_size)
+  with Trainer.from_pickle(config) if os.path.exists(path) else Trainer(
+      config=config,
+      make_agent=agents.make,
+      make_env=lambda: make_env(config),
+      task_sampler=task_sampler) as trainer:
+    objective, constraint = trainer.train()
+  assert np.asarray(objective.values()).mean() > 100.0
