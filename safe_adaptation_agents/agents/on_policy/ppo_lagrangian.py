0377913c6401f4c7bddbf2f851bcb73db4c6bc89
yardenas <yardas@student.ethz.ch>
2022-06-09 12:22:17 +0200

    Use new actor state

--- safe_adaptation_agents/agents/on_policy/ppo_lagrangian.py
+++ safe_adaptation_agents/agents/on_policy/ppo_lagrangian.py
@@ -81,7 +81,7 @@ class PpoLagrangian(safe_vpg.SafeVanillaPolicyGradients):
           self.policy_loss)(actor_state.params, observation, action, advantage,
                             cost_advantage, lagrangian, old_pi_logprob)
       new_actor_state = self.actor.grad_step(grads, actor_state)
-      pi = self.actor.apply(actor_state.params, observation)
+      pi = self.actor.apply(new_actor_state.params, observation)
       kl_d = old_pi.kl_divergence(pi).mean()
       return iter_ + 1, new_actor_state, {
           'agent/actor/loss': loss,
