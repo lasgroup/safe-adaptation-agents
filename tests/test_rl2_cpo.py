36df79be57f12d5621a0be53dbd1d4fe08b22478
yardenas <yardas@student.ethz.ch>
2022-07-03 14:15:51 +0200

    No need such a good critic

--- tests/test_rl2_cpo.py
+++ tests/test_rl2_cpo.py
@@ -117,7 +117,7 @@ def test_cheetah():
       '--configs', 'defaults', '--agent', 'rl2_cpo', '--eval_trials', '0',
       '--epochs', '1000', '--render_episodes', '0', '--log_dir',
       'results/test_rl2_cpo_half_cheetah', '--task_batch_size', '16', '--safe',
-      'False', '--actor.layers', '[128, 128]', '--critic.layers', '[128, 128]',
+      'False', '--actor.layers', '[128, 128]', '--critic.layers', '[64, 64]',
       '--time_limit', '200', '--num_trajectories', '20', '--train_driver',
       '{\'adaptation_steps\': 2000, \'query_steps\': 2000}', '--test_driver',
       '{\'adaptation_steps\': 2000, \'query_steps\': 2000}', '--jit', 'True',
