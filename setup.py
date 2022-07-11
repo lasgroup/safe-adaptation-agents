6ca0a65ccff228798d23c4b600847ead7d60692c
yardenas <yardas@student.ethz.ch>
2022-07-11 10:30:13 +0200

    Package rename

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 required = [
-    'safe_adaptation_gym', 'jax', 'dm-haiku', 'optax', 'jmp', 'numpy',
+    'safe-adaptation-gym', 'jax', 'dm-haiku', 'optax', 'jmp', 'numpy',
     'ruamel.yaml', 'tensorboardX', 'tensorflow', 'tensorflow-probability',
-    'moviepy', 'tesorflow_datasets'
+    'moviepy', 'tensorflow-datasets'
 ]
 
 extras = {'dev': ['pytest>=4.4.0', 'Pillow', 'matplotlib', 'mujoco-py']}
 
 setup(
-    name='safe_adaptation_agents',
+    name='safe-adaptation-agents',
     version='0.0.0',
     packages=find_packages(),
     python_requires='>3.8',
