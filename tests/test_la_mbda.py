f68a7593bea602ffe36f7093a7fde3e96b175326
yardenas <yardas@student.ethz.ch>
2022-07-14 17:19:39 +0200

    Upper bound estimation

--- tests/test_la_mbda.py
+++ tests/test_la_mbda.py
@@ -32,7 +32,7 @@ def test_run():
       '--update_steps', '2', '--train_every', '100', '--parallel_envs', '1',
       '--swag',
       '{start_averaging: 1, average_period: 1, max_num_models: 1, decay: 0.8, scale: 1.}',
-    '--action_repeat', '1'
+      '--action_repeat', '1', '--posterior_samples', '2'
   ])
   if not config.jit:
     from jax.config import config as jax_config
