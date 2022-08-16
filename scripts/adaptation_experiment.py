03a6cdf2428b1ff438d9a24ccb4a0b5e2db79de5
yardenas <yardas@student.ethz.ch>
2022-08-16 11:28:35 +0200

    Fix bug with action repeat

--- scripts/adaptation_experiment.py
+++ scripts/adaptation_experiment.py
@@ -1,7 +1,5 @@
 import os
 
-from gym.wrappers import TimeLimit
-
 from safe_adaptation_gym import benchmark
 
 from safe_adaptation_agents import agents
@@ -11,11 +9,12 @@ from safe_adaptation_agents.trainer import Trainer
 
 def make_env(config):
   import safe_adaptation_gym
+  from safe_adaptation_agents import wrapppers
   env = safe_adaptation_gym.make(
       config.robot,
       render_options=config.render_options,
       render_lidar_and_collision=config.render_lidar_and_collision)
-  env = TimeLimit(env, config.time_limit)
+  env = wrapppers.ActionRepeat(env, config.action_repeat)
   return env
 
 
